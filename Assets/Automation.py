ab66a9412b85dd76a390e1b458e9f30bbb79f6dc
c0deGIO <106058428+c0deGIO@users.noreply.github.com>
2023-10-15 13:33:32 +0300

    Update Automation.py

--- Assets/Automation.py
+++ Assets/Automation.py
@@ -7,8 +7,6 @@ with open("Chars.txt", "r", encoding="utf-8") as f:
     for i in data:
         chars[i[0]] = i[1]
 
-
-
 def toGreek(s):
     st = ""
     keys = chars.keys()
@@ -19,12 +17,11 @@ def toGreek(s):
             st += i.upper()
     return st
 
-
 data = pd.read_excel("spreadsheet.xlsx")
 
 def getTeachers(data: pd.read_excel):
     teachers = []
-    for i in range(34):
+    for i in range(35):
         teachers.append(toGreek(data.iloc[1+i, 0]))
     print(teachers)
     with open("Teachers.json", "w") as f:
@@ -35,7 +32,7 @@ def getClasses(data: pd.read_excel):
     classes = []
     for ix in range(35):
         temp = []
-        for iy in range(34):
+        for iy in range(35):
             t = str(data.iloc[1+iy, ix+1])
             if t == "nan":
                 temp.append([])
@@ -60,7 +57,8 @@ def getClasses(data: pd.read_excel):
         else:
             if i.startswith("Β"):
                 if len(i) == 2:
-                    cl["second"][1].append(i)
+                    if i[1].isdigit():
+                        cl["second"][1].append(i)
                 else:
                     cl["third"][1].append(i)
             if i.startswith("Γ"):
@@ -68,10 +66,10 @@ def getClasses(data: pd.read_excel):
                     cl["second"][2].append(i)
                 else:
                     cl["third"][2].append(i)
-
     for i in range(3):
         cl["second"][i].sort()
         cl["third"][i].sort()
+    print(cl)
 
     with open("SelectClass.json", "w") as f:
         json.dump(cl, f)
